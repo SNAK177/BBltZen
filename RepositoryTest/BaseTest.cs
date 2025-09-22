8cf9fb7a2b58e79d74366122cdc0031b8177e5ac
SNAK177 <alessandrorufini99@gmail.com>
2025-09-22 16:43:36 +0200

    fix

--- RepositoryTest/BaseTest.cs
+++ RepositoryTest/BaseTest.cs
@@ -15,8 +15,17 @@ namespace RepositoryTest
             build.Services.AddTransient<IOrdineRepository, OrdineRepository>();
             build.Services.AddTransient<IDolceRepository, DolceRepository>();
             build.Services.AddTransient<IUtentiRepository, UtentiRepository>();
-
-
+            build.Services.AddTransient<INotificheOperativeRepository, NotificheOperativeRepository>();
+            build.Services.AddTransient<IStatoPagamentoRepository, StatoPagamentoRepository>();
+            build.Services.AddTransient<IStatoOrdineRepository, StatoOrdineRepository>();
+            build.Services.AddTransient<ITavoloRepository, TavoloRepository>();
+            build.Services.AddTransient<ITaxRatesRepository, TaxRatesRepository>();
+            build.Services.AddTransient<ISessioniQrRepository, SessioniQrRepository>();
+            build.Services.AddTransient<IIngredienteRepository, IngredienteRepository>();
+            //build.Services.AddTransient<IVwIngredientiPopolariRepository, VwIngredientiPopolariRepository>();
+            build.Services.AddTransient<IVwStatisticheOrdiniAvanzateRepository, VwStatisticheOrdiniAvanzateRepository>();
+            build.Services.AddTransient<IOrderItemRepository, OrderItemRepository>();
+            
             build.Services.AddServiceDb();
             build.Services.AddDbContext<BubbleTeaContext>();
             build.Services.AddDbContext<Database.BubbleTeaContext>();
