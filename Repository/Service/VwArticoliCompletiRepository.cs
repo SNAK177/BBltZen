a3404cb2bd4d4d9fb8bb9251de4daae5f15fb470
SNAK177 <alessandrorufini99@gmail.com>
2025-10-17 10:10:17 +0200

    b

--- Repository/Service/VwArticoliCompletiRepository.cs
+++ Repository/Service/VwArticoliCompletiRepository.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository.Service
 {
@@ -16,6 +17,19 @@ namespace Repository.Service
         {
             _context = context;
         }
-        public async Task<IEnumerable<VwArticoliCompletiDTO>>
+
+        public async Task<IEnumerable<VwArticoliCompletiDTO>> GetAllAsync()
+        {
+            return (IEnumerable<VwArticoliCompletiDTO>)await _context.VwArticoliCompleti
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<VwArticoliCompleti?> GetByIdAsync(int id)
+        {
+            return await _context.VwArticoliCompleti
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.ArticoloId == id);\
+        }
     }
 }
