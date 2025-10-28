fdd58d734711f96eaf96accedc7898d1346186ee
SNAK177 <alessandrorufini99@gmail.com>
2025-10-28 11:54:08 +0100

    a

--- frontend-react/src/pages/CartCheckout.jsx
+++ frontend-react/src/pages/CartCheckout.jsx
@@ -1,1 +1,3 @@
-export default function CartCheckout(){return <div className='container'><h2>Carrello</h2></div>}
+export default function CartCheckout() {
+    return <div className='container'><h2>Carrello</h2></div>
+}
